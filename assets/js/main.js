75bacc58b47d97820351283a1a0bee5d417f56ad
dev-prime99 <prime99@icloud.com>
2019-10-23 17:06:10 +0200

    completed logic structure

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,21 +3,22 @@ var cognomi = ["", "rossi", "verdi", "gialli", "bianco" ];
 
 console.log(cognomi);
 
-var utente = cognomi.push(prompt("Inserisci il tuo cognome"));
+var utente = (prompt("Inserisci il tuo cognome")).toLowerCase();
+
+var inserimento = cognomi.push(utente);
+
+console.log(utente);
 
 console.log(cognomi);
 
 var i = 1;
 
-
 while (i < cognomi.length) {
   cognomi.sort();
-  // Prima lettera maiuscola
-  cognomi[i] = cognomi[i].toLowerCase()
-      .split(' ')
-      .map((s) => s.charAt(0).toUpperCase() + s.substring(1))
-      .join(' ');
 
   document.getElementById('list').innerHTML += "<li>" + cognomi[i] + "</li>";
+
+  console.log(cognomi.indexOf(utente));
+
   i++;
 }
